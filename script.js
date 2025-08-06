0559214218274ca074a96c56f9d43b249d1f8922
baronejavier <74462330+baronejavier@users.noreply.github.com>
2025-08-06 08:40:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,15 +19,18 @@ function enviarPedido(event) {
   const nombre = document.getElementById("nombre").value.trim();
   const muza8 = document.getElementById("muza8").value;
   const napo8 = document.getElementById("napo8").value;
+  const fuga = document.getElementById("fuga").value;
+  const cala = document.getElementById("cala").value;
   const mitadmitad = document.getElementById("mitadmitad").value;
   const sierra = document.getElementById("sierra").value;
   const lata = document.getElementById("lata").value;
+  const brahma = document.getElementById("brahma").value;
   const c361 = document.getElementById("c361").value;
   const agua = document.getElementById("agua").value;
   const vino = document.getElementById("vino").value;
   const secco = document.getElementById("secco").value;
 
-  if (muza8 === "0" && napo8 === "0" && mitadmitad === "0") {
+  if (muza8 === "0" && napo8 === "0" && fuga === "0" && cala === "0" && mitadmitad === "0") {
     alert("Seleccioná al menos una pizza para continuar.");
     return;
   }
@@ -45,6 +48,14 @@ function enviarPedido(event) {
     mensaje += `- ${napo8} Napolitana de 8 porciones ($8.500 c/u). `;
   }
 
+  if (fuga !== "0") {
+    mensaje += `- ${fuga} Fugazzeta de 8 porciones ($8.000 c/u). `;
+  }
+
+  if (cala !== "0") {
+    mensaje += `- ${cala} Calabresa de 8 porciones ($9.500 c/u). `;
+  }
+
   if (mitadmitad !== "0") {
     mensaje += `- ${mitadmitad} Mitad Mozzarella / Mitad Napolitana ($8.000 c/u). `;
   }
@@ -54,7 +65,11 @@ function enviarPedido(event) {
   }
 
   if (lata !== "0") {
-    mensaje += `- ${lata} Cerveza Heineken 473 mL ($2.500 c/u). `;
+    mensaje += `- ${lata} Cerveza Heineken 473 mL ($3.500 c/u). `;
+  }
+
+  if (brahma !== "0") {
+    mensaje += `- ${brahma} Cerveza Brahma 473 mL ($1.800 c/u). `;
   }
 
   if (c361 !== "0") {
@@ -77,6 +92,8 @@ function enviarPedido(event) {
   const totalPizzas = 
     (muza8 === "+3" ? 4 : parseInt(muza8)) +
     (napo8 === "+3" ? 4 : parseInt(napo8)) +
+    (fuga === "+3" ? 4 : parseInt(fuga)) +
+    (cala === "+3" ? 4 : parseInt(cala)) +
     (mitadmitad === "+3" ? 4 : parseInt(mitadmitad));
 
   if (totalPizzas >= 2) {
